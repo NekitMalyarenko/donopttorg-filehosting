bf2bda706707c9f59323e3020f58d9f63684aa92
NekitMalyarenko <n.a.m.62608@gmail.com>
2019-06-13 17:40:34 +0300

    1.0

---
+++ server/server.go
@@ -0,0 +1,74 @@
+package server
+
+import (
+	"os"
+	"log"
+	"github.com/valyala/fasthttp"
+	"strings"
+)
+
+const (
+	defaultPort    = "8080"
+	webContentPath = "resources/"
+)
+
+var (
+	allowedTypes = []string{"css", "js", "png", "jpg", "svg", "jpeg"}
+)
+
+
+func Start() {
+	fasthttp.ListenAndServe(GetPort(), Handler)
+}
+
+
+func Handler(ctx *fasthttp.RequestCtx) {
+	url := ctx.URI().String()
+	log.Println(url)
+
+	if strings.Contains(url, "://") && strings.Contains(url, "/") {
+		url = string([]byte(url)[strings.Index(url, "://") + len("://") + 1:])
+		url = string([]byte(url)[strings.Index(url, "/") + len("/"):])
+		path := ""
+		isAllowed := false
+
+		if url == "" {
+			ctx.SendFile(webContentPath + "index.html")
+			return
+		}
+
+		for _, val := range allowedTypes {
+			if strings.Contains(url, "." + val) {
+				isAllowed = true
+				break
+			}
+		}
+
+		if isAllowed {
+			path = webContentPath + url
+
+			if _, err := os.Stat(path); os.IsNotExist(err) {
+				ctx.NotFound()
+			} else {
+				ctx.SendFile(path)
+				ctx.Response.Header.Add("Cache-Control", "no-cache, no-store, must-revalidate")
+			}
+
+		} else {
+			ctx.NotFound()
+		}
+
+	} else {
+		ctx.NotFound()
+	}
+}
+
+
+func GetPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	return ":" + port
+}
