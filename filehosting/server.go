0db83997c42eccfe60467a85a6eb1074f7cb2ec4
NekitMalyarenko <n.a.m.62608@gmail.com>
2019-06-24 18:50:57 +0300

    24.06

--- filehosting/server.go
+++ filehosting/server.go
@@ -5,23 +5,102 @@ import (
 	"log"
 	"github.com/valyala/fasthttp"
 	"strings"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/qiangxue/fasthttp-routing"
+	"net/http"
+	"bytes"
+	"strconv"
 )
 
 var (
 	webContentPath = ""
-	allowedTypes = []string{"css", "js", "png", "jpg", "svg", "jpeg"}
+	allowedTypes = []string{"css", "js", "png", "jpg", "svg", "jpeg", "ico"}
+
+	proxyContentPath = ""
+	proxyData = map[string]string{}
+	trustedExternalResources = []string{"https://donopttorg-api.herokuapp.com", "https://fonts.googleapis.com", "https://kit.fontawesome.com"}
 )
 
 
 func GetFileHostingHandler(contentPath string) func(ctx *fasthttp.RequestCtx) {
-	webContentPath = contentPath + "/"
-	return Handler
+	webContentPath   = contentPath + "/"
+	proxyContentPath = contentPath + "/proxyContent/"
+
+	os.Mkdir(proxyContentPath, os.ModePerm)
+
+	route := routing.New()
+	route.NotFound(DefaultHandler)
+	route.Get("/proxy", ProxyHandler)
+
+	InitManifest()
+
+	return route.HandleRequest
 }
 
 
-func Handler(ctx *fasthttp.RequestCtx) {
-	url := ctx.URI().String()
-	log.Println(url)
+func ProxyHandler(c *routing.Context) error {
+	if c.Request.URI().QueryArgs().Peek("url") == nil || len(c.Request.URI().QueryArgs().Peek("url")) == 0 {
+		c.Error("url is empty", http.StatusBadRequest)
+		return nil
+	}
+
+	url := string(c.Request.URI().QueryArgs().Peek("url"))
+
+	for k, v := range proxyData {
+		if k == url {
+			c.SendFile(v)
+			c.Response.Header.Add("Cache-Control", "no-cache, no-store, must-revalidate")
+			return nil
+		}
+	}
+
+	fileName := proxyContentPath + strconv.Itoa(len(proxyData)) + ".txt"
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer([]byte{}))
+	if err != nil {
+		log.Println(err)
+		c.Error("can't load resource", http.StatusInternalServerError)
+		return nil
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		c.Error("can't load resource", http.StatusInternalServerError)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Println(err)
+		c.Error("can't load resource", http.StatusInternalServerError)
+		return nil
+	}
+	file.Close()
+
+	err = ioutil.WriteFile(fileName, body, 0644)
+	if err != nil {
+		log.Println(err)
+		c.Error("can't load resource", http.StatusInternalServerError)
+		return nil
+	}
+
+	proxyData[url] = fileName
+
+	c.Write(body)
+	c.Response.Header.Add("Cache-Control", "no-cache, no-store, must-revalidate")
+	return nil
+}
+
+
+func DefaultHandler(c *routing.Context) error {
+	url := c.URI().String()
+	//log.Println(url)
 
 	if strings.Contains(url, "://") && strings.Contains(url, "/") {
 		url = string([]byte(url)[strings.Index(url, "://") + len("://") + 1:])
@@ -30,8 +109,8 @@ func Handler(ctx *fasthttp.RequestCtx) {
 		isAllowed := false
 
 		if url == "" || !strings.Contains(url, ".") {
-			ctx.SendFile(webContentPath + "index.html")
-			return
+			c.SendFile(webContentPath + "index.html")
+			return nil
 		}
 
 		for _, val := range allowedTypes {
@@ -43,19 +122,100 @@ func Handler(ctx *fasthttp.RequestCtx) {
 
 		if isAllowed {
 			path = webContentPath + url
+			log.Println(path)
 
 			if _, err := os.Stat(path); os.IsNotExist(err) {
-				ctx.NotFound()
+				c.NotFound()
+				return nil
 			} else {
-				ctx.SendFile(path)
-				ctx.Response.Header.Add("Cache-Control", "no-cache, no-store, must-revalidate")
+				c.SendFile(path)
+				if !strings.Contains(url, ".svg") {
+					c.Response.Header.Add("Cache-Control", "no-cache, no-store, must-revalidate")
+				}
+
+				return nil
 			}
 
 		} else {
-			ctx.NotFound()
+			c.NotFound()
+			return nil
 		}
 
 	} else {
-		ctx.NotFound()
+		c.NotFound()
+		return nil
+	}
+}
+
+
+func InitManifest() {
+	log.Println(webContentPath + "manifest.json")
+
+	if _, err := os.Stat(webContentPath + "manifest.json"); !os.IsNotExist(err) {
+		log.Println("manifest found")
+
+		rawData, err := ioutil.ReadFile(webContentPath + "manifest.json")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		var parsedData map[string]interface{}
+		err = json.Unmarshal(rawData, &parsedData)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		resources := make([]string, 0)
+
+		var handlerDir func(dirPath string)
+		handlerDir = func(dirPath string) {
+			files, err := ioutil.ReadDir(dirPath)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			for _, file := range files {
+
+				if file.IsDir() {
+					handlerDir(dirPath + file.Name() + "/")
+				} else if file.Name() != "manifest.json" && file.Name() != "serviceWorker.js" &&
+					!strings.Contains(file.Name(), "fasthttp.gz") {
+
+					var name string
+
+					if file.Name() == "index.html" {
+						name = "/"
+					} else {
+						name = dirPath + file.Name()
+						name = strings.Replace(name, webContentPath, "/", 1)
+					}
+
+					resources = append(resources, name)
+				}
+			}
+		}
+
+		handlerDir(webContentPath)
+		parsedData["resources"] = resources
+		//parsedData["trustedExternalResources"] = trustedExternalResources
+
+		rawData, err = json.Marshal(parsedData)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		err = ioutil.WriteFile(webContentPath + "manifest.json", rawData, 0644)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println("end")
+	} else {
+		log.Println("manifest not found")
 	}
 }
--- resources/serviceWorker.js
+++ resources/serviceWorker.js
@@ -1,48 +1,122 @@
 const prefix = "[Service Worker]";
+const CACHE_NAME = "resources";
+const second = 1000;
 
-self.addEventListener('install', () => {
-    log("Install");
+
+self.addEventListener('install', (event) => {
+    log("Install", self.registration.scope);
+
+    setTimeout(updateResources, 60 * second);
+
+    event.waitUntil(
+        loadResources()
+            .then(() => {
+                log("resources are loaded");
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    )
 });
 
 
 self.addEventListener('fetch', (event) => {
-    log(`fetch ${event.request}`);
-    return fetch(event.request);
+    //console.log({event});
+    //return fetch(event.request);
+    let url = event.request.url;
+
+    const defaultFetch = () => {
+        fetch(event.request)
+            .then(result => {
+                if (result.ok && !event.request.url.includes("/manifest.json") && event.request.method === 'GET') {
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, result);
+                        })
+                }
+                return result.clone();
+            })
+    };
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                } else if (url.includes(self.registration.scope) && !url.includes(".")) {
+                    return caches.match(new Request("/"))
+                        .then((response) => {
+                            if (response) {
+                                return response;
+                            } else {
+                                log("might be in cache, but not found");
+                                return defaultFetch();
+                            }
+                        })
+                } else {
+                   return defaultFetch();
+                }
+            })
+    );
 });
 
 
-self.addEventListener('message', (event) => {
-    switch (event.type) {
+async function loadResources(manifest) {
+    let res, cache = await caches.open(CACHE_NAME);
 
-        case "cacheCheck":
-
-            if (event.payload.localStorage) {
-                fetch("/manifest.json").then((response) => {
-                    let json = response.json();
-                    log(`manifest : ${json}`);
-
-                    if (json.type === "beta"){
-                        return;
-                    }
-
-                    if (event.payload.localStorage.getItem("manifest") && event.payload.localStorage.getItem("manifest") !== "") {
-                        let prevManifest = JSON.parse(event.payload.localStorage.getItem("manifest"));
-                        log(`prevManifest : ${prevManifest}`);
-
-                        if ((prevManifest.type === "beta" && json.type !== "beta") || (json.version >  prevManifest.version)) {
-                            event.payload.localStorage.setItem("manifest", JSON.stringify(json));
-
-
-                        }
-                    } else {
-                        event.payload.localStorage.setItem("manifest", JSON.stringify(json))
-                    }
-
-                });
-            }
-            break
+    if (manifest) {
+        res = manifest;
+    } else {
+        res = await fetch("/manifest.json");
+        cache.put(new Request("lastFetchedManifest.json"), res.clone());
     }
-});
+
+    let parsedRes =  await res.json();
+    let resources = parsedRes.resources;
+
+    resources.forEach((url) => {
+        log(`adding to cache: ${url}`);
+        const request = new Request(url);
+
+        fetch(request, {method: 'GET'})
+            .then((res) => {
+                if (res.ok) {
+                    cache.put(request, res.clone());
+                } else {
+                    log('not ok')
+                }
+
+                return res;
+            })
+    })
+
+}
+
+
+async function updateResources() {
+    try {
+        let res = await fetch("/manifest.json"),
+            clone = res.clone(),
+            cache = await caches.open(CACHE_NAME),
+            prevManifest = await cache.match(new Request("lastFetchedManifest.json"));
+
+        let parsedRes = await clone.json(),
+            parsedPrevRes = await prevManifest.json();
+
+        await cache.put(new Request("lastFetchedManifest.json"), res.clone());
+
+        if (parsedRes.version > parsedPrevRes.version) {
+            await caches.delete(CACHE_NAME);
+            await loadResources(res);
+        }
+
+        setTimeout(updateResources, 10 * second);
+    } catch (error) {
+        console.error(error);
+        setTimeout(updateResources, 10 * second);
+    }
+}
 
 
 function log(text) {
